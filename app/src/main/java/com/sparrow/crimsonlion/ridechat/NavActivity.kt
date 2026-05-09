3f8126db49fe4e853473693208fa21aac88886bf
Ankit <ankit.steven007@gmail.com>
2026-05-09 18:50:14 +0530

    adding the compose setConentView

--- app/src/main/java/com/sparrow/crimsonlion/ridechat/NavActivity.kt
+++ app/src/main/java/com/sparrow/crimsonlion/ridechat/NavActivity.kt
@@ -3,11 +3,19 @@ package com.sparrow.crimsonlion.ridechat
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.view.ViewCompat
 
 class NavActivity: AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView){ _, insets -> insets }
+        setContentView(
+            ComposeView(this).apply {
+
+            }
+        )
     }
 }
