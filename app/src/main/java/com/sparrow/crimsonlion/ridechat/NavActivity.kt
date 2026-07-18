3209c2012b8def4abd2d8ee72cdb10c59e8c352a
Ankit <ankit.steven007@gmail.com>
2026-07-18 18:57:29 +0530

    added the app bar in compose chat app

--- app/src/main/java/com/sparrow/crimsonlion/ridechat/NavActivity.kt
+++ app/src/main/java/com/sparrow/crimsonlion/ridechat/NavActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.ComposeView
 import androidx.core.view.ViewCompat
+import com.sparrow.crimsonlion.ridechat.components.RideChatAppBar
+import com.sparrow.crimsonlion.ridechat.dialog.RideChatAlertDialog
+import com.sparrow.crimsonlion.ui.theme.CrimsonLionTheme
 
 class NavActivity: AppCompatActivity(){
 
@@ -14,7 +20,19 @@ class NavActivity: AppCompatActivity(){
         super.onCreate(savedInstanceState)
         //ViewCompat.setOnApplyWindowInsetsListener(window.decorView){ _, insets -> insets }
         setContent {
-
+            CrimsonLionTheme {
+                ShowInitialScreen()
+            }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShowInitialScreen() {
+    RideChatAppBar(
+        title = {
+            Text("Your's Ride Chat")
+        }
+    )
 }
---
+++ app/src/main/java/com/sparrow/crimsonlion/ridechat/dialog/DialogCardComponent.kt
@@ -0,0 +1,25 @@
+package com.sparrow.crimsonlion.ridechat.dialog
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RideChatAlertDialog(onDismiss: () -> Unit) {
+  AlertDialog(
+    onDismissRequest = onDismiss,
+    text = {
+        Text(
+           text = "This Feature is not available \uD83D\uDE48",
+            style = MaterialTheme.typography.bodyMedium
+        )
+    },
+    confirmButton = {
+        TextButton(onClick = onDismiss) {
+            Text("Close")
+        }
+    },
+  )
+}
