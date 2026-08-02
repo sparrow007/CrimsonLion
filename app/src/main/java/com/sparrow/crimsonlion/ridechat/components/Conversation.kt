cae2629e2eb8128f21a8d469c568e18bda3f99ff
Ankit <ankit.steven007@gmail.com>
2026-08-03 00:58:26 +0530

    update the conversation

--- app/src/main/java/com/sparrow/crimsonlion/ridechat/components/Conversation.kt
+++ app/src/main/java/com/sparrow/crimsonlion/ridechat/components/Conversation.kt
@@ -3,6 +3,10 @@ package com.sparrow.crimsonlion.ridechat.components
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -14,6 +18,10 @@ fun ChannelNameBar(
     scrollBehaviour: TopAppBarScrollBehavior? = null,
     onNaveIconPressed: () -> Unit = {}
 ) {
+    var functionalNotAvailableShow by remember { mutableStateOf(false) }
+    if (functionalNotAvailableShow) {
+
+    }
 
 
 
