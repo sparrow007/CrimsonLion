43977763906cb47dd9e5d5c96f1f8fac4fcfc310
Ankit <ankit.steven007@gmail.com>
2026-08-02 12:21:52 +0530

    update the conversation

--- app/src/main/java/com/sparrow/crimsonlion/MainActivity.kt
+++ app/src/main/java/com/sparrow/crimsonlion/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sparrow.crimsonlion.ridechat.components.RideChatAppBar
 import com.sparrow.crimsonlion.ui.theme.CrimsonLionTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CrimsonLionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = RideChatAppBar(),
+                    modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
---
+++ app/src/main/java/com/sparrow/crimsonlion/ridechat/components/Conversation.kt
@@ -0,0 +1,20 @@
+package com.sparrow.crimsonlion.ridechat.components
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChannelNameBar(
+    channelName: String,
+    channelMembers: String,
+    modifier: Modifier = Modifier,
+    scrollBehaviour: TopAppBarScrollBehavior? = null,
+    onNaveIconPressed: () -> Unit = {}
+) {
+
+
+
+}
