55f58f01133ac29b36fa850d615b95aaa4d1c536
Ankit <ankit.steven007@gmail.com>
2026-08-04 01:21:22 +0530

    added drag and drop with some state in converstation

--- app/src/main/java/com/sparrow/crimsonlion/ridechat/components/Conversation.kt
+++ app/src/main/java/com/sparrow/crimsonlion/ridechat/components/Conversation.kt
@@ -2,26 +2,98 @@ package com.sparrow.crimsonlion.ridechat.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draganddrop.DragAndDropEvent
+import androidx.compose.ui.draganddrop.DragAndDropTarget
+import androidx.compose.ui.draganddrop.toAndroidDragEvent
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.sparrow.crimsonlion.R
+import com.sparrow.crimsonlion.ridechat.converstation.ConversationUiState
+import com.sparrow.crimsonlion.ridechat.converstation.Message
 import com.sparrow.crimsonlion.ridechat.dialog.FunctionalityNotAvailablePopup
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ConversationContent() {
+fun ConversationContent(
+    uiState: ConversationUiState,
+    navigateToProfile: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    onNaveIconPressed: () -> Unit
+) {
+    val authorMe = stringResource(R.string.author_me)
+    val timeNow = stringResource(id = R.string.now)
+
+    val scrollState = rememberLazyListState()
+    val topBarState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(topBarState)
+    val scope = rememberCoroutineScope()
+
+    var background by remember {
+        mutableStateOf(Color.Transparent)
+    }
+
+    var borderStroke by remember {
+        mutableStateOf(Color.Transparent)
+    }
+
+    val dragAndDropCallback = remember {
+        object : DragAndDropTarget {
+            override fun onDrop(event: DragAndDropEvent): Boolean {
+                val clipData = event.toAndroidDragEvent().clipData
+
+                if (clipData.itemCount < 1) {
+                    return false
+                }
+
+                uiState.addMessage(
+                    Message(
+                        authorMe,
+                        clipData.getItemAt(0).text.toString(),
+                        timeNow
+                    )
+                )
+                return true
+            }
+
+            override fun onStarted(event: DragAndDropEvent) {
+                super.onStarted(event)
+                borderStroke = Color.Red
+            }
+
+            override fun onEntered(event: DragAndDropEvent) {
+                super.onEntered(event)
+                background = Color.Red.copy(alpha = .3f)
+            }
+
+            override fun onExited(event: DragAndDropEvent) {
+                super.onExited(event)
+                background = Color.Transparent
+            }
+
+            override fun onEnded(event: DragAndDropEvent) {
+                super.onEnded(event)
+                background = Color.Transparent
+                borderStroke = Color.Transparent
+            }
+        }
+    }
 
 }
 
